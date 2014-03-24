82f89969d949da3d4ff133731e6d734051b9ddf0
Yaroslav Kravchenko <yaroslav.kravchenko@hotmail.com>
2014-03-24 14:54:25 +0400

    Fixed resources names

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var create = function () {
 	var units = new UnitSet();
 
 	units.addSet('tree', tree.create());
-	units.addSet('item', item.create());
-	units.add('tag', new Tags());
+	units.addSet('tree.item', item.create());
+	units.add('tree.tags', new Tags());
 
 	return units;
 };
